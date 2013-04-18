db5d780b7fab9e3d90a896f40469c2f602b68e23
klarrimore <klarrimore@icehook.com>
2013-04-18 01:00:17 -0400

    added install check

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'klarrimore@icehook.com'
 license          'All rights reserved'
 description      'Installs/Configures OpenSIPS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 recipe "opensips", "Installs opensips package and sets up configuration"
 recipe "opensips::source", "Installs opensips from source and sets up configuration"
