4cbf011fab6ee006074decb528474bb246b4e996
Daniel Jabbour <daniel@amoe.ba>
2013-09-10 18:55:20 -0700

    Updating URL due to 404

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['opensips']['prefix'] = '/usr'
 
-default['opensips']['source']['url'] = "http://opensips.org/pub/opensips/latest/src/opensips-1.9.1-svn10242_src.tar.gz"
+default['opensips']['source']['url'] = "http://opensips.org/pub/opensips/1.9.1/src/opensips-1.9.1-svn10142_src.tar.gz"
 default['opensips']['source']['include_modules'] = ["signaling,", "sl,", "tm,", "rr,", "dialog,", "maxfwd,", "usrloc,", "registrar,", "textops,", "sipmsgops,", "mi_fifo,", "uri,", "auth,", "auth_db,", "nathelper,", "alias_db,", "group,", "rtpproxy,", "enum,", "avpops,", "pike,", "domain"]
 
 # Memory options (inserted into /etc/default) for shared memory (s) and package memory (p), in Mb
